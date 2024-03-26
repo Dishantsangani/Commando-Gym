ae97257a09c4778ef6894926eb877de7a527c33c
Dishant Sangani <128277319+Dishantsangani@users.noreply.github.com>
2024-03-26 13:46:13 +0530

    Added index.html

---
+++ script.js
@@ -0,0 +1,31 @@
+function clearErrors() {
+  errors = document.getElementsByClassName("formerror");
+  for (let item of errors) {
+    item.innerHTML = "";
+  }
+}
+function seterror(id, error) {
+  element = document.getElementById(id);
+  element = getElementsByClassName("formerror")[0].innerHTML = error;
+}
+function validateform() {
+  var returnval = true;
+  clearErrors();
+  var name = document.forms["myForm"]["fname"].value;
+  if (name.length > 5) {
+    seterror("name", "* Length of name is too short");
+    returnval = false;
+  }
+  if (name.length == 0) {
+    seterror("name", "* Length of name cannot be zero");
+    returnval = false;
+  }
+
+  var email = document.forms["myForm"]["femail"].value;
+  if (email.length > 15) {
+    seterror("email", "*Email length is too long");
+    returnval = false;
+  }
+
+  return returnval;
+}
